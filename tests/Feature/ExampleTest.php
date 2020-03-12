89fb639062cd837b48e1731466d17c7695a5186c
nishizuka <nishizuka@spookies.co.jp>
2020-03-12 13:18:34 +0000

    #1 テストを動かせるように改修

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+        //        print_r(substr($response->getContent(), 0, 1000));
+        //        $response->assertStatus(200);
+        $response->assertStatus(500);
+    }
+}
